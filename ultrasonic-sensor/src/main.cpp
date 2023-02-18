f65688c9848604c0aec3f8925bd1c7f80008eee2
kimanigitau01 <kimmanasseh14@gmail.com>
2023-02-19 00:03:14 +0300

    add an alarm sequence functiom

--- ultrasonic-sensor/src/main.cpp
+++ ultrasonic-sensor/src/main.cpp
@@ -1,7 +1,11 @@
-/* this program is an alarm that works together with the leds to ring whenever ther is an intruder*/
+/* this program is an alarm that works together with the leds to ring whenever ther is an intruder
+Written by: Manasseh Gitau
+*/
+
 #include <Arduino.h>
 
 int distance = 0;
+int duration  = 0;
 
 const int trig = 2;
 const int echo = 3;
@@ -13,6 +17,7 @@ const int greenLed = 9;
 
 void setup() {
   Serial.begin(9600);
+  
   pinMode(redLed,OUTPUT);
   pinMode(greenLed,OUTPUT);
 
@@ -20,8 +25,43 @@ void setup() {
 
   pinMode(trig,OUTPUT);
   pinMode(echo, INPUT);
+
+  digitalWrite(redLed, LOW);
+  digitalWrite(greenLed, HIGH);
 }
 
 void loop() {
+  digitalWrite(trig, LOW);
+  delay(2);
+  digitalWrite(trig, HIGH);
+  delay(2);
+  digitalWrite(trig, LOW);
 
+  duration = pulseIn(echo, HIGH);
+  distance =  (duration * 0.034) / 2;
+
+  if (distance <= 20){
+    alarmSequence(alarm, led);
+
+  }
+}
+
+void alarmSequence(int device, int detectLed, int neutralLed){
+  tone(device, 100);
+  delay(10);
+  tone(device, 200);
+  delay(10);
+  tone(device, 300);
+  delay(10);
+
+  digitalWrite(detectLed, HIGH);
+  delay(500);
+  digitalWrite(redLed, LOW);
+  delay(500);
+  digitalWrite(redLed, HIGH);
+  delay(500);
+  digitalWrite(redLed, LOW);
+  delay(500);
+  digitalWrite(greenLed, LOW);
+  
 }
