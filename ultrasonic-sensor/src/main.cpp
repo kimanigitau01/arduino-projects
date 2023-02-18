33067906e91636f793a94e3a865a22806d0d7642
kimanigitau01 <kimmanasseh14@gmail.com>
2023-02-18 23:48:46 +0300

    add new variables and serial monitor

--- ultrasonic-sensor/src/main.cpp
+++ ultrasonic-sensor/src/main.cpp
@@ -1,6 +1,7 @@
 /* this program is an alarm that works together with the leds to ring whenever ther is an intruder*/
 #include <Arduino.h>
 
+int distance = 0;
 
 const int trig = 2;
 const int echo = 3;
@@ -11,9 +12,16 @@ const int redLed = 8;
 const int greenLed = 9;
 
 void setup() {
-  
+  Serial.begin(9600);
+  pinMode(redLed,OUTPUT);
+  pinMode(greenLed,OUTPUT);
+
+  pinMode(alarm,OUTPUT);
+
+  pinMode(trig,OUTPUT);
+  pinMode(echo, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
 }
