9a475369fa4feb2bc7203274883a22c9f4d96c00
kimanigitau01 <kimmanasseh14@gmail.com>
2023-02-18 23:45:45 +0300

    add new variables

--- ultrasonic-sensor/src/main.cpp
+++ ultrasonic-sensor/src/main.cpp
@@ -1,7 +1,17 @@
+/* this program is an alarm that works together with the leds to ring whenever ther is an intruder*/
 #include <Arduino.h>
 
+
+const int trig = 2;
+const int echo = 3;
+
+const int alarm = 7;
+
+const int redLed = 8;
+const int greenLed = 9;
+
 void setup() {
-  // put your setup code here, to run once:
+  
 }
 
 void loop() {
