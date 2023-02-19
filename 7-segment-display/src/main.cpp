ec7836d6eed550f561503eef45e81637bfe2fb95
kimanigitau01 <kimmanasseh14@gmail.com>
2023-02-19 16:12:19 +0300

    add the byte digits for the 7-segment display

--- 7-segment-display/src/main.cpp
+++ 7-segment-display/src/main.cpp
@@ -1,7 +1,30 @@
 #include <Arduino.h>
 
+int segmentPin[] = {2, 3, 4, 5, 6, 7, 8};
+
+byte digits [10][8] = {
+
+  // a, b, c, d, e, f, g
+  {1,1,1,1,1,1,0}, //0
+  {0,1,1,0,0,0,0}, //1
+  {1,1,0,1,0,0,1}, //2
+  {1,1,1,1,0,0,1}, //3
+  {0,1,1,0,0,1,1}, //4
+  {1,0,1,1,1,0,1}, //5
+  {1,0,1,1,1,1,1}, //6
+  {1,1,1,0,0,0,0}, //7
+  {1,1,1,1,1,1,1}, //8
+  {1,1,1,1,0,1,1}, //9
+
+};
 void setup() {
-  // put your setup code here, to run once:
+  for (int i = 0; i <8; i++){
+    pinMode(segmentPin[i], OUTPUT);
+    digitalWrite(segmentPin[i], LOW);
+  }
+Serial.begin(9600);
+Serial.println("Enter a number between 0 and 9: ");
+
 }
 
 void loop() {
